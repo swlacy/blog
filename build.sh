e74a9af900174b7a393f7e7c1a511a829b9b81d8
Sid Lacy <contact@swlacy.com>
2022-11-17 16:33:14 -0800

    build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+
+hugo server --bind=0.0.0.0\
+            --port 80\
+            --buildDrafts\
+            --buildExpired\
+            --buildFuture   
+            --minify\
+            --enableGitInfo\
+            --gc
