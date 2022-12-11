3b12acf6da0d2e55c30e36c96031f69c62bc34a9
Sid Lacy <contact@swlacy.com>
2022-12-11 02:44:56 -0800

    clean

--- .BACKUP/build.sh
+++
@@ -1,4 +0,0 @@
-#/usr/bin/env bash
-
-screen -dmS hugo bash -c "hugo server --bind=0.0.0.0 --buildDrafts --buildExpired --buildFuture --enableGitInfo --gc"
-screen -dmS tailwind bash -c "cd themes/hugo-port-theme/tailwindcss && ./tailwindcss -i main.css -o ../static/css/style.min.css --watch --minify"
