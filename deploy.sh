242b19ecb5b5833d7a32a2aa6fc4952bce79b100
Sid Lacy <contact@swlacy.com>
2023-03-19 05:10:15 +0000

    add checks/status to deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,30 @@ cd ~/projects/blog
 # Optimize images
 # From https://virtubox.github.io/img-optimize/
 if test -f "$HOME/.img-optimize/optimize.sh"; then
-    cd static/media && bash $HOME/.img-optimize/optimize.sh --all
-    rename s/\.png// *.png.webp && rename s/\.jpg// *.jpg.webp
+    echo "[!] Optimizing images"
+    
+    # Run optimization script
+    cd static/media && bash $HOME/.img-optimize/optimize.sh --all 2>/dev/null
+    
+    # Rename converted files to .webp
+    rename s/\.png// *.png.webp && rename s/\.jpg// *.jpg.webp 2>/dev/null
+    
+    # Update references to images in content
     cd ../../content
-    grep -rl '.png' . | xargs sed -i 's/png/webp/g'
-    grep -rl '.jpg' . | xargs sed -i 's/jpg/webp/g'
+    grep -rl '.png' . | xargs sed -i 's/png/webp/g' 2>/dev/null
+    if [ $? -ne 0 ]; then
+        echo '[!] No *.png references to update'
+    fi
+    grep -rl '.jpg' . | xargs sed -i 's/jpg/webp/g' 2>/dev/null
+    if [ $? -ne 0 ]; then
+        echo '[!] No *.jpg references to update'
+    fi
+    
     cd ..
     echo "[!] Done optimizing images"
 fi
 
-# Get commit message
+# Get/prompt for commit message
 if [ $# -gt 0 ]; then
     msg="$*"
 else
@@ -23,15 +37,18 @@ fi
 if [ -z "$msg" ]; then
     msg="No commit message provided"
 fi
+echo "[!] Base commit message: $msg"
+
+# Build and commit changes to blog
+rm -rf public
+hugo --minify --gc && git add . && git commit -m "$msg" && git push
+echo "[!] Done pushing blog"
 
 # Build and commit changes to theme
 cd themes/hugo-port-theme
 git add . && git commit -m "[PARENT BUILD] $msg" && git push
-
-# Build and commit changes to blog
 cd ../..
-rm -rf public
-hugo --minify --gc && git add . && git commit -m "$msg" && git push
+echo "[!] Done pushing theme"
 
-# Deploy to Firebase
+# Deploy live
 firebase deploy
